7ffc1ffb54e7acc8075251573ac640f0a840ec7e
Miguel Mexicano Herrera <miguelmexicano18@gmail.com>
2023-11-09 16:46:37 -0600

    add image view

--- SecondApp/SecondApp/ContentView.swift
+++ SecondApp/SecondApp/ContentView.swift
@@ -10,12 +10,43 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "recordingtape")
-                .imageScale(.large)
-                .foregroundColor(.green)
-                .font(.system(size: 80))
-                .shadow(radius: 10)
-            Text("Hello, world!")
+            Image("new-york")
+                .resizable()
+                //.edgesIgnoringSafeArea(.top)
+                //.scaledToFit()
+                //.aspectRatio(contentMode: .fill)
+                //.aspectRatio(contentMode: .fit)
+                //.frame(width: 200)
+                //.clipped()
+                //.clipShape(Circle())
+                .opacity(0.9)
+                .overlay(
+                    /*Image(systemName: "heart.fill")
+                        .font(.system(size: 60))
+                        .foregroundColor(.red)
+                        .opacity(0.8)*/
+                    /*Text("Que ganas de volver a New York sentir el frio en invierno y pasear por Central Park con un buen bagel en la mano!")
+                        .fontWeight(.bold)
+                        .font(.system(.headline, design: .rounded))
+                        .foregroundColor(.white)
+                        .padding()
+                        .background(.gray)
+                        .cornerRadius(10)
+                        .opacity(0.7)
+                        .padding(),
+                    alignment: .top*/
+                    Rectangle()
+                        .foregroundColor(.gray)
+                        .opacity(0.3)
+                        .overlay(
+                            Text("New York")
+                                .font(.title)
+                                .fontWeight(.bold)
+                                .foregroundColor(.white)
+                        )
+                        )
+            Text("New York!")
+            Text("test")
         }
         .padding()
     }
